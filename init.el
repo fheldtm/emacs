4ce7c0043fe09b9cb5e21fbb1d37281153204c5c
fheldtm <fheldtm@github.com>
2024-09-26 18:47:16 +0900

    update init

--- init.el
+++ init.el
@@ -17,16 +17,3 @@
 (prefer-coding-system 'utf-8)
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("12db058ce4ba460e067e331a67dbb05c4406d8c0d5e4504cebc059cffae55672" "6742a3aa17b810c7c16af5fcad85ee76ed12923a6822285f7c02537eb0e53a57" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
